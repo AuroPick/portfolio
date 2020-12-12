85a9c42b168755e3dbcb6cfd6b89ba96b52a9e44
Aykut Saki <aykutsakisocial@gmail.com>
2020-12-12 21:46:15 +0300

    beautify code

--- public/js/card-toggle-script.js
+++ public/js/card-toggle-script.js
@@ -1,20 +1,7 @@
-let card = document.querySelectorAll(".card")[0];
-let card2 = document.querySelectorAll(".card")[1];
-let card3 = document.querySelectorAll(".card")[2];
-let card4 = document.querySelectorAll(".card")[3];
+const cards = document.querySelectorAll(".card");
 
-card.addEventListener("click", () => {
+cards.forEach(card => {
+  card.addEventListener("click", () => {
     card.classList.toggle("toggle");
-})
-
-card2.addEventListener("click", () => {
-    card2.classList.toggle("toggle");
-})
-
-card3.addEventListener("click", () => {
-    card3.classList.toggle("toggle");
-})
-
-card4.addEventListener("click", () => {
-    card4.classList.toggle("toggle");
-})
+  });
+});
